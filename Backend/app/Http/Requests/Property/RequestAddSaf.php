d3ef9846b05a5448392f5d08734eb94a8c5d4329
SandeepBara <sandeepbara830@gmail.com>
2026-01-05 11:07:23 +0530

    remove builtupArea requird

--- Backend/app/Http/Requests/Property/RequestAddSaf.php
+++ Backend/app/Http/Requests/Property/RequestAddSaf.php
@@ -119,7 +119,7 @@ class RequestAddSaf extends ParentRequest
             "plotNo"=>"required",
             "villageMaujaName"=>"required",
             "areaOfPlot"=>"required|numeric|min:0.1",
-            "builtupArea"=>"required|numeric|min:0".($this->areaOfPlot ? "|max:".decimalToSqFt($this->areaOfPlot) : ""),
+            "builtupArea"=>"nullable|required_unless:propTypeMstrId,4|numeric|min:0".($this->areaOfPlot ? "|max:".decimalToSqFt($this->areaOfPlot) : ""),
             "propAddress"=>"required",
             "propCity"=>"required",
             "propDist"=>"required",
